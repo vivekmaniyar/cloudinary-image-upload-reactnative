696a4cd91d7a84a0acba44fdcf42202611f45f8f
Vivek Maniyar <vivekmaniyar9601@gmail.com>
2022-06-05 11:07:59 +0530

    Added project files

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import { useState } from 'react';
+import { Avatar } from '@rneui/themed';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const[image, setImage] = useState(null);
+
+  const handleImagePicker = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  const uploadImage = async () => {
+    const data = new FormData();
+    data.append('file', {
+      uri: image,
+      name: 'image.jpg',
+      type: 'image/jpg',
+      });
+    data.append('upload_preset', 'UploadPreset');
+
+    const response = await fetch('https://api.cloudinary.com/v1_1/CloudName/image/upload', {
+      method: 'POST',
+      body: data,
+    });
+
+    const responseData = await response.json();
+
+    if(response.ok) {
+      console.log("-----UPLOAD SUCCESS-----");
+      console.log("URL for API: ",("v"+responseData.version+"/"+responseData.public_id+"."+responseData.format));
+      console.log("full URL: ",responseData.secure_url);
+    }else{
+      console.log("-----UPLOAD FAILED-----");
+      console.log("Error: ",responseData.error);
+    }
+  };
+    
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {image && <Avatar size={100} rounded source={{ uri: image }} />}
+      <Button title='Pick an Image' onPress={() =>handleImagePicker()} />
+      <Button title='Upload Image' onPress={() =>uploadImage()} />
     </View>
   );
 }
